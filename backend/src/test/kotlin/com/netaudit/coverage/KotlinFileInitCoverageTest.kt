9095db246439b062aa3e7f18b58d4f8b76c6f454
Mercy <1651159045@qq.com>
2026-03-09 20:56:13 +0800

    test: 覆盖Kotlin文件初始化

---
+++ backend/src/test/kotlin/com/netaudit/coverage/KotlinFileInitCoverageTest.kt
@@ -0,0 +1,26 @@
+package com.netaudit.coverage
+
+import kotlin.test.Test
+
+class KotlinFileInitCoverageTest {
+    @Test
+    fun `load kotlin file classes`() {
+        val classes = listOf(
+            "com.netaudit.pipeline.AuditPipelineKt",
+            "com.netaudit.parser.http.HttpParserKt",
+            "com.netaudit.stream.TcpStreamTrackerKt",
+            "com.netaudit.parser.dns.DnsParserKt",
+            "com.netaudit.parser.telnet.TelnetParserKt",
+            "com.netaudit.parser.ftp.FtpParserKt",
+            "com.netaudit.alert.AlertEngineKt",
+            "com.netaudit.storage.BatchWriterKt",
+            "com.netaudit.storage.DatabaseFactoryKt",
+            "com.netaudit.capture.PacketCaptureEngineKt",
+            "com.netaudit.parser.ParserRegistryKt"
+        )
+
+        classes.forEach { className ->
+            Class.forName(className)
+        }
+    }
+}
